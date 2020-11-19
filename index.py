af2988d5a5228d2cdbfbfa74851e94569d4f2746
Zuhair Naqi <38198958+zuhairnaqi@users.noreply.github.com>
2020-11-19 16:51:20 +0500

    Fill the missing data in all columns

--- index.py
+++ index.py
@@ -17,8 +17,26 @@ print("---------------- Line Break ---------------------")
 print(newFrame['Two'].isnull())
 print(newFrame[newFrame['Two'].isnull()])
 
+print("---------------- Line Break ---------------------")
+
 # Replace missing data
 print(newFrame['Two'].fillna("Zuhair"))
 
+print("---------------- Line Break ---------------------")
+
 #Drop missing data
 print(newFrame['One'].dropna())
+
+print("---------------- Line Break ---------------------")
+
+#Fill the null data in columns with specific value
+for col in newFrame:
+    newFrame[col] = newFrame[col].fillna('filled')
+    
+print("new Frame", newFrame)
+
+
+
+
+
+
